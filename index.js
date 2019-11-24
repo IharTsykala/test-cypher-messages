59cb4e0a9f706394c90a9df312391dbf10578ca9
Ihar Tsykala <Sudja09@bk.ru>
2019-11-24 19:56:55 +0300

    feat: add function description

---
+++ index.js
@@ -0,0 +1,33 @@
+const textArea = document.querySelector("#textArea")
+const button = document.querySelector("#button")
+const answerArea = document.querySelector("#answerArea")
+
+const decryptionTextArea = () => {
+  const bufferString = textArea.value.toLowerCase()
+
+  if (bufferString.length > 100000) return "More 100000 letters"
+
+  const bufferArray = bufferString.trim().split("")
+  const textAnswerArea = []
+
+  bufferArray.forEach((item, index) => {
+    if (
+      (bufferArray[index] !== bufferArray[index + 1] &&
+        bufferArray[index] !== bufferArray[index - 1]) ||
+      (bufferArray[index] === bufferArray[index + 1] &&
+        bufferArray[index] === bufferArray[index - 1])
+    ) {
+      textAnswerArea.push(item)
+    }
+  })
+  console.log(textAnswerArea)
+  return textAnswerArea.join("")
+}
+
+const fillAnswerArea = () => {
+  answerArea.innerHTML = decryptionTextArea()
+}
+
+button.addEventListener("click", () => {
+  fillAnswerArea(textArea.value)
+})
